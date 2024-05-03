12f09879c73894c47836a1cf443794cd3ece1f1a
Carlos Vivas <carsed@hotmail.com>
2024-05-03 03:52:03 -0400

    update controller to upload files and update user in mongo db

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -22,23 +22,35 @@ export const patchUserDocuments = async (req, res) => {
     const user = await userModel.findOne({ _id: uid });
     if (!user) return res.status(400).send({ status: "error", error: `User not found` });
 
+    let documents = user.documents.filter((d) => d.name !== "profile");
+
     const profile = req.files["profile"]
       ? req.files["profile"][0].path.split("public").join("")
       : "";
+    if (profile) documents = [...documents, { name: "profile", reference: profile }];
 
     const identity = req.files["identity"]
       ? req.files["identity"][0].path.split("public").join("")
       : "";
+    if (identity) documents = [...documents, { name: "identity", reference: identity }];
 
     const residence = req.files["identity"]
       ? req.files["residence"][0].path.split("public").join("")
       : "";
+    if (residence) documents = [...documents, { name: "residence", reference: residence }];
 
     const account = req.files["account"]
       ? req.files["account"][0].path.split("public").join("")
       : "";
+    if (account) documents = [...documents, { name: "account", reference: account }];
 
     const product = req.files["product"] ?? [];
+    product.forEach((p) => {
+      documents = [...documents, { name: "product", reference: p.path.split("public").join("") }];
+    });
+
+    user.documents = documents;
+    await user.save();
 
     res.send({ status: "success", message: "Success" });
   } catch (error) {
